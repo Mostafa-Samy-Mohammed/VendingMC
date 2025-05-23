93d9a27560feeb966f3a67229261988d63f5ff70
Mostafa-Samy-Mohammed <mostafasmy020@gmail.com>
2025-05-23 17:15:23 +0300

    Add project files

---
+++ Product.h
@@ -0,0 +1,35 @@
+/**
+ * @file Product.h
+ *
+ * @authors Eng. Mostafa Samy Mohammed,
+ *          Eng. Ahmed Osama Elgendy,
+ *          Eng. Ingy Ahmed Sherif
+ *
+ * @brief   This header file contains the declaration of the Product class-
+ *          -which is used to store the information of a product.
+ *          The information includes the name of the product, the price, its count, expiration date,-
+ *          -and number of outlet the product come out of.
+ * @version 0.4
+ * @date 2023-12-30
+*/
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include "LIB.h"
+
+using namespace std;
+
+
+class Product
+{
+public:
+    string name;
+    float price;
+    int count;
+    string expirationDate;
+    int outlet;
+
+    Product(string _name, float _price, int _count, string _expirationDate, int _outlet);
+    Product();
+};
+#endif
---
+++ VendingMachine.h
@@ -0,0 +1,36 @@
+/**
+ * @file VendingMachine.h
+ *
+ * @authors Eng. Mostafa Samy Mohammed,
+ *          Eng. Ahmed Osama Elgendy,
+ *          Eng. Ingy Ahmed Sherif
+ *
+ * @brief This header file contains the declaration of the VendingMachine class methods.
+ * @version 0.6
+ * @date 2023-12-30
+ */
+#ifndef VENDINGMC_H
+#define VENDINGMC_H
+
+#include "LIB.h"
+#include "Product.h"
+
+using namespace std;
+
+class VendingMachine
+{
+private:
+
+    vector<Product> products;
+
+public:
+    bool checkExpirationDate(Product *product) const;
+    void displayProducts() const;
+    void addProduct();
+    void removeProduct();
+    void viewDefectProducts() const;
+    void chooseProduct();
+    void predefinedProducts();
+    void editPrice();
+};
+#endif
